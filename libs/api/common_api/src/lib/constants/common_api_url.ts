3b785d2a3520c9e452fa8b385267e63be299dbbb
omkar-bandal <omkarbandal2010@gmail.com>
2024-11-18 16:04:00 +0530

    url changed again

--- libs/api/common_api/src/lib/constants/common_api_url.ts
+++ libs/api/common_api/src/lib/constants/common_api_url.ts
@@ -1,4 +1,4 @@
 export const COM_API_URL = {
     //Authentication
-    BASE_URL: "ec2-13-234-20-64.ap-south-1.compute.amazonaws.com:80",
+    BASE_URL: "http://ec2-13-234-20-64.ap-south-1.compute.amazonaws.com:80",
 }
